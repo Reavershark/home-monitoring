5097f6feb32dd50f24cdba7374101dc6ca0c31e5
Jonas Meeuws <jonas.meeuws@gmail.com>
2023-05-18 20:30:43 +0200

    .ino files: use 2 spaces for indentation

--- arduino/http_sender_example/http_sender_example.ino
+++ arduino/http_sender_example/http_sender_example.ino
@@ -32,96 +32,96 @@ void loop();
 //////////////////////////////
 
 #ifdef USE_SERIAL
-    #define SERIAL_CALL(arg) Serial.arg
+  #define SERIAL_CALL(arg) Serial.arg
 #else
-    #define SERIAL_CALL(arg)
+  #define SERIAL_CALL(arg)
 #endif
 
 void connect_wifi()
 {
-    while (1)
-    {
-        SERIAL_CALL(print("Connecting to wifi with SSID: "));
-        SERIAL_CALL(println(wifi_ssid));
+  while (1)
+  {
+    SERIAL_CALL(print("Connecting to wifi with SSID: "));
+    SERIAL_CALL(println(wifi_ssid));
 
-        if (WiFi.begin(wifi_ssid, wifi_pass) != WL_CONNECTED)
-        {
-            SERIAL_CALL(print("Failed to connect, retrying in "));
-            SERIAL_CALL(println(wifi_retry_connect_delay_msecs));
-            SERIAL_CALL(println("ms"));
-            delay(wifi_retry_connect_delay_msecs);
-            continue;
-        }
-        else
-        {
-            SERIAL_CALL(println("Successfully connected to wifi"));
-            break;
-        }
+    if (WiFi.begin(wifi_ssid, wifi_pass) != WL_CONNECTED)
+    {
+      SERIAL_CALL(print("Failed to connect, retrying in "));
+      SERIAL_CALL(println(wifi_retry_connect_delay_msecs));
+      SERIAL_CALL(println("ms"));
+      delay(wifi_retry_connect_delay_msecs);
+      continue;
     }
+    else
+    {
+      SERIAL_CALL(println("Successfully connected to wifi"));
+      break;
+    }
+  }
 }
 
 void setup()
 {
-    SERIAL_CALL(begin(9600));
-    connect_wifi();
-    tcp_client.connect(http_server_address, http_server_port);
+  SERIAL_CALL(begin(9600));
+  connect_wifi();
+  tcp_client.connect(http_server_address, http_server_port);
 }
 
 void loop()
 {
-    // Check if wifi is still connected
-    if (!WiFi.status() != WL_CONNECTED)
+  // Check if wifi is still connected
+  if (!WiFi.status() != WL_CONNECTED)
+  {
+    // Wifi was disconnected, try to reconnect
+    connect_wifi();
+  }
+
+  // Read and discard any available data
+  while (tcp_client.read() != -1)
+  {
+  }
+
+  // Check if the tcp client is still connected
+  if (!tcp_client.connected())
+  {
+    tcp_client.stop();
+    tcp_client.connect(http_server_address, http_server_port);
+  }
+
+  unsigned long curr_timestamp_msecs = millis();
+  unsigned long time_since_last_send = curr_timestamp_msecs - last_send_timestamp_msecs;
+  if (time_since_last_send >= send_interval_msecs) {
+    last_send_timestamp_msecs = curr_timestamp_msecs;
+
+    int value = 1 + (millis() % 789) / 789.0;
+
+    // Create json object to send
+    // Use https://arduinojson.org/v6/assistant to get the recommended size
+    // Example json: See readme file
+    String json_string;
+    StaticJsonDocument<192> json;
     {
-        // Wifi was disconnected, try to reconnect
-        connect_wifi();
+      json["measurement"] = "water_depth";
+      json["tags"]["location"] = "some_canal";
+      json["fields"]["depth_in_meters"] = value;
+      json["bucket"] = "default";
     }
-
-    // Read and discard any available data
-    while (tcp_client.read() != -1)
+    serializeJson(doc, json_string);
+    
+    String http_message;
     {
+      http_message += String("POST /somepath HTTP/1.1\n");
+
+      // Headers
+      http_message += String("Host: ") + String(http_server_address) + String("\n");
+      http_message += String("Content-Length: ") + String(json_string.length()) + String("\n");
+      http_message += String("\n"); // Indicate end of headers with an empty line
     }
 
-    // Check if the tcp client is still connected
-    if (!tcp_client.connected())
-    {
-        tcp_client.stop();
-        tcp_client.connect(http_server_address, http_server_port);
-    }
-
-    unsigned long curr_timestamp_msecs = millis();
-    unsigned long time_since_last_send = curr_timestamp_msecs - last_send_timestamp_msecs;
-    if (time_since_last_send >= send_interval_msecs) {
-        last_send_timestamp_msecs = curr_timestamp_msecs;
-
-        int value = 1 + (millis() % 789) / 789.0;
-
-        // Create json object to send
-        // Use https://arduinojson.org/v6/assistant to get the recommended size
-        // Example json: See readme file
-        String json_string;
-        StaticJsonDocument<192> json;
-        {
-            json["measurement"] = "water_depth";
-            json["tags"]["location"] = "some_canal";
-            json["fields"]["depth_in_meters"] = value;
-            json["bucket"] = "default";
-        }
-        serializeJson(doc, json_string);
-        
-        String http_message;
-        {
-            http_message += String("POST /somepath HTTP/1.1\n");
-
-            // Headers
-            http_message += String("Host: ") + String(http_server_address) + String("\n");
-            http_message += String("Content-Length: ") + String(json_string.length()) + String("\n");
-            http_message += String("\n"); // Indicate end of headers with an empty line
-        }
-
-        tcp_client.print(http_message);
-    }
-    else
-    {
-        delay(10);
-    }
+    tcp_client.print(http_message);
+  }
+  else
+  {
+    delay(10);
+  }
 }
