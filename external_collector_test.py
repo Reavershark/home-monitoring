7fda1823856c3c9ec3f3ae018748da8d065d6d1d
Jonas Meeuws <jonas.meeuws@gmail.com>
2023-05-19 13:28:49 +0200

    Update external_collector_test.py

--- external_collector_test.py
+++ external_collector_test.py
@@ -1,15 +1,21 @@
-import requests, random
+import requests, random, time
 
-example = {
-  "measurement": "water_depth",
-  "tags": {"location": "some_canal"},
-  "fields": {"depth_in_meters": random.uniform(1, 2)},
-  "bucket": "default"
-}
-
-res = requests.post(
-  url="http://localhost:8080/",
-  json=example
-)
-
-print(res)
+while True:
+    try:
+        example = {
+          "measurement": "water_depth",
+          "tags": {"location": "some_canal"},
+          "fields": {"depth_in_meters": random.uniform(1, 2)},
+          "bucket": "default",
+          #"time": str(int(time.time() * 1_000_000_000))
+        }
+        res = requests.post(
+          url="http://home-monitoring-pi:8080/",
+          json=example
+        )
+        print(res)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        print("retrying in 10s")
+        time.sleep(10)
